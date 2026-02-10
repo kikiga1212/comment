8c81a3ec60b9c23ae4475ba3102a3aae4a68532b
kant300 <p.o.i.u.0987552@gmail.com>
2026-02-10 18:04:06 +0900

    list, detail

--- src/main/java/com/example/comment/Controller/CommentRestController.java
+++ src/main/java/com/example/comment/Controller/CommentRestController.java
@@ -37,12 +37,12 @@ public class CommentRestController {
 //@Controller는 model로 값을 전달하고, return으로 이동페이지
 //public String(이동페이지의 문자열)
 //DTO전달 받을때 @ModelAttribute를 선언하거나 생략
-//매핑 :  GetMapping(목록, 상세보기,삽입/수정페이지), PostMapping(삽입처리,수정처리, 삭제처리), DeleteMapping(삭제)
+//매핑 :  GetMapping(목록, 상세보기,삽입/수정페이지), PostMapping(삽입처리,수정처리, 삭제처리)
 //@RestController는 model로 값을 전달하지 않고, return으로 json데이터를 전달
 //(이동페이지는 호출한 페이지로 이동)
 //public 전달할 값의 유형
 //DTO전달 받을때 @RequestBody를 선언
 //매핑 : GetMapping(목록,상세)
 //       PostMapping(삽입처리)
-//       PutMapping(
+//       PutMapping(수정처리-전체),
 //       DeleteMapping(
